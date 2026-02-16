604febeed2bf560dd42c05a6c141d8b8009929e1
loaded7 <thomasmodesto2005@gmail.com>
2026-02-16 12:38:42 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Alterado para './' para garantir que os links funcionem em qualquer subpasta ou dominio
-  base: './',
+  // Define o caminho base como o nome do repositório para garantir que o GitHub Pages encontre os arquivos
+  base: '/espacovida/',
   server: {
     host: true,
     port: 5173
