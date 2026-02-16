ef99b31cf75fbf3b8d329500f053cbf44da44875
loaded7 <thomasmodesto2005@gmail.com>
2026-02-16 12:19:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Alterado para './' para garantir que os links funcionem em qualquer subpasta ou dominio
+  base: './',
+  server: {
+    host: true,
+    port: 5173
+  }
 })
